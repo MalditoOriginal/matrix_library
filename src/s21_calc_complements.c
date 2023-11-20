74e680049ed8325c7534daf36e4c3ab685be95ea
MalditoOriginal <maldito.goldpride@gmail.com>
2023-11-20 22:00:26 +0300

    Add files via upload

--- src/s21_calc_complements.c
+++ src/s21_calc_complements.c
@@ -1,30 +1,42 @@
 #include "s21_matrix.h"
 
 int s21_calc_complements(matrix_t *A, matrix_t *result) {
-    int ret = 0, i = 0, j = 0;
-    
+    int ret = 0;
+
     if (matrixWrong(A) == 0) {
         if (A->rows == A->cols) {
             ret = s21_create_matrix(A->rows, A->cols, result);
+            
             if (ret == 0) {
-                for (i = 0; i < A->rows; i++) {
-                    for (j = 0; j < A->cols; j++) {
-                        matrix_t flags;
-                        double res = 0;
-                        rmRowCol(A, &flags, i, j);
-                        ret = s21_determinant(&flags, &res);
+                for (int i = 0; i < A->rows; i++) {
+                    for (int j = 0; j < A->cols; j++) {
+                        matrix_t cofactor;
+                        double determinant = 0;
+                        
+                        // Calculate the determinant of the submatrix
+                        rmRowCol(A, &cofactor, i, j);
+                        ret = s21_determinant(&cofactor, &determinant);
+                        s21_remove_matrix(&cofactor);
+
                         if (ret == 0) {
-                            result->matrix[i][j] = res * pow(-1, j + i);
+                            // Apply the cofactor formula
+                            result->matrix[i][j] = determinant * pow(-1, i + j);
+                        } else {
+                            // Error in determinant calculation
+                            s21_remove_matrix(result);
+                            return ret;
                         }
-                        s21_remove_matrix(&flags);
                     }
                 }
             }
         } else {
+            // Not a square matrix
             ret = 2;
         }
     } else {
+        // Invalid matrix
         ret = 1;
     }
+
     return ret;
 }
--- src/s21_create.c
+++ src/s21_create.c
@@ -1,24 +1,26 @@
 #include "s21_matrix.h"
 
 int s21_create_matrix(int rows, int cols, matrix_t *result) {
-    int ret = 0, i;
+    int ret = 0; // Assume success initially
 
     if (rows <= 0 || cols <= 0) {
-        ret = 1;
+        ret = 1; // Invalid matrix dimensions
     } else {
         result->rows = rows;
         result->cols = cols;
         result->matrix = (double **)calloc(rows, sizeof(double *));
 
         if (result->matrix == NULL) {
-            ret = 1;
+            ret = 1; // Memory allocation failed for rows
         } else {
-            for (i = 0; i < rows; i++) {
+            for (int i = 0; i < rows; i++) {
                 result->matrix[i] = (double *)calloc(cols, sizeof(double));
 
                 if (result->matrix[i] == NULL) {
-                    ret = 1;
+                    // Memory allocation failed for a row, clean up and set error
                     s21_remove_matrix(result);
+                    ret = 1;
+                    break; // Exit the loop on failure
                 }
             }
         }
@@ -28,14 +30,14 @@ int s21_create_matrix(int rows, int cols, matrix_t *result) {
 }
 
 void s21_remove_matrix(matrix_t *A) {
-    int i;
     if (A != NULL) {
         if (A->matrix != NULL) {
-            for (i = 0; i < A->rows; i++) {
+            for (int i = 0; i < A->rows; i++) {
                 free(A->matrix[i]);
+                A->matrix[i] = NULL; // Set the pointer to NULL after freeing
             }
             free(A->matrix);
-            A->matrix = NULL;            
+            A->matrix = NULL; // Set the pointer to NULL after freeing
         }
         A->rows = 0;
         A->cols = 0;
